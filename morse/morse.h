219319da4ec4f0b5708ff80f3e9b2a1d6da7be5a
LeXxleloxx <31853868+LeXxleloxx@users.noreply.github.com>
2017-12-11 13:03:04 -0200

    v3

--- morse/morse.h
+++ morse/morse.h
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+
 struct TNodoA{
     int numero;
     struct TNodoA *esq;
@@ -5,18 +9,20 @@ struct TNodoA{
 };
 typedef struct TNodoA pNodoA;
 
-#define MAX 10
 
 struct AlfabetoMorse{
     char letra;
-    struct AlfabetoMorse *ant;
-    struct AlfabetoMorse *pos;
-    char *cod[];
+    struct AlfabetoMorse *esq;
+    struct AlfabetoMorse *dir;
+    char cod[MAX];
 };
 typedef struct AlfabetoMorse Morse;
 
 
 Morse* InicializaABP(void);
-Morse* InsereABP(Morse *a, int chave);
-void ler_morse (Morse a);
-Morse* consulta(Morse *a, int chave);
+Morse* InsereABP(Morse *a, int chave, char codaux);
+void ler_morse (Morse* a);
+Morse* consulta(Morse *a, int chave,int *comp);
+void copia_arquivo(Morse *a, int *comp);
+char remove_acento( char c);
+void ImprimePreFix(Morse *a);
