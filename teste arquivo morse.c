e40b77e062cbc34aecc11f1a1eabb4e8c732a339
LeXxleloxx <31853868+LeXxleloxx@users.noreply.github.com>
2017-12-11 12:59:56 -0200

    Update teste arquivo morse.c

--- teste arquivo morse.c
+++ teste arquivo morse.c
@@ -4,81 +4,19 @@
 void main()
 {
     FILE* arq;
+    int i=0;
     char c;
     arq=fopen("TabelaMorse.txt","r");
     if (arq == NULL)
         printf("Erro na leitura do arquivo\n");
 
-        c=fgetc(arq);
-    printf(" %c\n",c);
-        c=fgetc(arq);
-    printf("%c\n",c);
-        c=fgetc(arq);
-    printf("%c\n",c);
+    //fseek(arq,6* sizeof(char),SEEK_SET);
+
+
+for(i=0;i<10;i++){
+        c=getc(arq);
+    printf("%i - %c \n",i,c);
 }
 
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "morse.h"
-#include <locale.h>
-
-void ler_morse ()
-{
-    char ch; //ponteiro que vai percorrendo o arquivo
-    Morse *tabela; //estrutura da tabela morse
-	setlocale(LC_ALL, "Portuguese");
-
-    FILE *arq;
-    arq = fopen("TabelaMorse.txt", "r"); //abre o arquivo
-
-    if (arq == NULL) //se não conseguiu abrir
-    {
-        printf("Problemas na leitura do arquivo\n");
-        return;
-    }
-    else{
-        while((ch = fgetc(arq)) != EOF){
-            if (ch == 'A'){
-                Morse *novo;
-                novo->ant = NULL;
-                novo->pos = NULL;
-                novo->letra = 'a';
-
-                char aux = 'a';
-                char *codaux[MAX];
-                int cont = 0;
-
-                while((ch = fgetc(arq)) == ' '){ //enquanto for espacos, prosseguir
-                    ch = fgetc(arq);
-                }
-                while((ch = fgetc(arq)) != '\n'){ //enquanto nao terminar a linha, guardar como o codigo morse
-                    codaux[cont] = fgetc(arq);
-                    cont = cont + 1;
-                }
-
-                novo->cod = codaux;
-
-                tabela = novo; //acrescenta na estrutura
-
-            }
-        }
-    }
-
-    fclose(arq);
 }
-
-Percorre_arq
-
-
-if(MorseTree == NULL){
-MorseTree->letra = fgetc(arq);
-MorseTree->esq = NULL;
-MorseTree->dir = NULL;
-arq++;
-MorseTree->codigo = fgetc(arq);
-
-
-
-
-A	,,,,,
+//    fseek(arq,1* sizeof(char),SEEK_CUR);
